1344c98030468acd78ec2a8921eaec9b7bb0da8a
Nick Hu <nick.hu@airteam.com.au>
2026-01-08 11:43:03 +0800

    fix: mobile bg

--- app/pageContent.tsx
+++ app/pageContent.tsx
@@ -22,8 +22,8 @@ const PageContent = () => {
   const showCompleteScreen = isComplete && !isSubmitting;
 
   return (
-    <div className="flex h-full items-center justify-centno bg-zinc-50 font-sans">
-      <div className="flex w-full max-w-lg flex-col justify-center bg-white p-8 mx-4">
+    <div className="flex h-full items-center justify-center bg-white md:bg-zinc-50 font-sans">
+      <div className="flex w-full max-w-lg flex-col justify-center bg-white p-8">
         {showCompleteScreen ? (
           <>
             <h2 className="text-2xl font-semibold mb-2 text-center">Hey {profile.username}, it&rsquo;s you again!</h2>
