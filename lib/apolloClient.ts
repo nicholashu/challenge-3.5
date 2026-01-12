c341465de282eda4b3ba00fdd90527673e333823
Nick Hu <nick.hu@airteam.com.au>
2026-01-12 09:29:20 +0800

    task: cache client fetch data

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -2,7 +2,12 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 const createApolloClient = () => {
   return new ApolloClient({
-    link: new HttpLink({ uri: "https://rickandmortyapi.com/graphql" }),
+    link: new HttpLink({ uri: "https://rickandmortyapi.com/graphql",
+      fetchOptions: {
+        // cache for 7 days as rick and morty API data changes infrequently
+        next: { revalidate: 604800 },
+      },
+     }),
     cache: new InMemoryCache(),
   });
 };
